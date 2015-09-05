8d8fd927a8add41e20466c765ec7419da2ddaeea
Siddharth Jha <sid@sidjha.com>
2015-09-05 03:04:08 -0400

    Implemented login request

--- mContactApp/LoginViewController.m
+++ mContactApp/LoginViewController.m
@@ -18,7 +18,62 @@
 @implementation LoginViewController
 
 - (IBAction)loginButtonPressed:(id)sender {
-    // TODO: make POST request to /users/login
+    // make request to /users/login
+    // Show a progress HUD
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.labelText = @"Logging in..";
+    
+    // Note: Explicitly getting a low priority queue and making the request on that,
+    //       because MBProgressHUD recommends so. However, the success and failure
+    //       methods are called on the main queue by AFNetworking anyway. So is
+    //       this even necessary? The progress HUD works even if called on the main
+    //       thread before the request. Do we need to do some garbage collection
+    //       on the main queue after?
+    
+    // new low priority thread to make request
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+        
+        NSString *username = _usernameField.text;
+        NSString *password = _passwordField.text;
+        NSDictionary *parameters = @{@"username": username, @"password": password};
+        NSString *URLString = @"http://127.0.0.1:5000/favor8/api/v1.0/users/login";
+        
+        // Set headers
+        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+        
+        manager.securityPolicy.allowInvalidCertificates = YES;
+        
+        manager.requestSerializer = [AFJSONRequestSerializer serializer];
+        
+        // Make the request
+        [manager
+         POST:URLString parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject){
+             NSLog(@"/users/login response data: %@", responseObject);
+             [MBProgressHUD hideHUDForView:self.view animated:YES];
+             
+             // TODO: save the auth token more securely in Keychain
+             NSString *authToken = responseObject[@"auth_token"];
+             NSString *userID = responseObject[@"userID"];
+             [[NSUserDefaults standardUserDefaults] setObject:authToken forKey:@"favor8AuthToken"];
+             [[NSUserDefaults standardUserDefaults] setObject:authToken forKey:@"favor8UserID"];
+             [[NSUserDefaults standardUserDefaults] synchronize];
+             
+             NSString *savedVal = [[NSUserDefaults standardUserDefaults] stringForKey:@"favor8AuthToken"];
+             NSLog(@"Saved value: %@", savedVal);
+             
+             // TODO: navigate to the logged-in view
+             
+         }failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+             NSLog(@"Error: %@", error);
+             
+             //dispatch_queue_t mainQueue = dispatch_get_main_queue();
+             //dispatch_async(mainQueue, ^{
+             [[[UIAlertView alloc] initWithTitle:@"Error" message:@"Sorry, we couldn't log you in. Check your internet connection." delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil] show];
+             // });
+             [MBProgressHUD hideHUDForView:self.view animated:YES];
+         }];
+    });
+
 }
 - (IBAction)signupButtonPressed:(id)sender {
     // make request to /users/create
@@ -51,7 +106,7 @@
         // Make the request
         [manager
          POST:URLString parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject){
-             NSLog(@"Submit response data: %@", responseObject);
+             NSLog(@"/users/create response data: %@", responseObject);
              [MBProgressHUD hideHUDForView:self.view animated:YES];
              
              // TODO: save the auth token more securely in Keychain
@@ -71,7 +126,7 @@
              
              //dispatch_queue_t mainQueue = dispatch_get_main_queue();
              //dispatch_async(mainQueue, ^{
-             [[[UIAlertView alloc] initWithTitle:@"Error" message:@"Sorry, we couldn't register you as a user" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil] show];
+             [[[UIAlertView alloc] initWithTitle:@"Error" message:@"Sorry, we couldn't register you as a user. Check your internet connection." delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil] show];
              // });
              [MBProgressHUD hideHUDForView:self.view animated:YES];
          }];
