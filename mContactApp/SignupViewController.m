72e93ef645ad8cd4365ee012e7ae9affcf089007
Siddharth Jha <sid@sidjha.com>
2015-10-17 19:05:55 -0400

    This fixes #59, fixes #36, fixes #79.

--- mContactApp/SignupViewController.m
+++ mContactApp/SignupViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "SignupViewController.h"
+#import "StackedViewController.h"
+
 #import "AFHTTPRequestOperationManager.h"
 #import "AFURLRequestSerialization.h"
+
 #import "MBProgressHUD.h"
-#import "StackedViewController.h"
 
 @interface SignupViewController ()
 
@@ -26,6 +28,8 @@
     self.usernameField.delegate = self;
     self.passwordField.delegate = self;
     self.nameField.delegate = self;
+    
+    [self.signupButton setEnabled:NO];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,64 +38,83 @@
 }
 
 - (IBAction)signupButtonPressed:(id)sender {
-    // make request to /users/create
+    
+    [self.view endEditing:YES];
+    
     // Show a progress HUD
     MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
-    hud.labelText = @"Signing you up...";
+ 
+    NSString *username = _usernameField.text;
+    NSString *password = _passwordField.text;
+    NSString *name = _nameField.text;
+    NSDictionary *parameters = @{@"username": username, @"password": password, @"name": name};
+    NSString *URLString = @"https://favor8api-alpha1.herokuapp.com/favor8/api/v1.0/users/create";
     
-    // Note: Explicitly getting a low priority queue and making the request on that,
-    //       because MBProgressHUD recommends so. However, the success and failure
-    //       methods are called on the main queue by AFNetworking anyway. So is
-    //       this even necessary? The progress HUD works even if called on the main
-    //       thread before the request. Do we need to do some garbage collection
-    //       on the main queue after?
+    // Set headers
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     
-    // new low priority thread to make request
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+    manager.securityPolicy.allowInvalidCertificates = NO;
+    
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    // Make the request
+    [manager
+     POST:URLString parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject){
+         
+         // TODO: save the auth token more securely in Keychain
+         NSString *authToken = responseObject[@"auth_token"];
+         NSString *userID = responseObject[@"user_id"];
+         [[NSUserDefaults standardUserDefaults] setObject:authToken forKey:@"favor8AuthToken"];
+         [[NSUserDefaults standardUserDefaults] setObject:userID forKey:@"favor8UserID"];
+         [[NSUserDefaults standardUserDefaults] synchronize];
+         
+         NSString *savedVal = [[NSUserDefaults standardUserDefaults] stringForKey:@"favor8AuthToken"];
+         
+         StackedViewController *controller = [self.storyboard instantiateViewControllerWithIdentifier:@"card"];
         
-        NSString *username = _usernameField.text;
-        NSString *password = _passwordField.text;
-        NSString *name = _nameField.text;
-        NSDictionary *parameters = @{@"username": username, @"password": password, @"name": name};
-        NSString *URLString = @"https://favor8api-alpha1.herokuapp.com/favor8/api/v1.0/users/create";
-        
-        // Set headers
-        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-        
-        manager.securityPolicy.allowInvalidCertificates = NO;
-        
-        manager.requestSerializer = [AFJSONRequestSerializer serializer];
-        
-        // Make the request
-        [manager
-         POST:URLString parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject){
-             NSLog(@"/users/create response data: %@", responseObject);
-             [MBProgressHUD hideHUDForView:self.view animated:YES];
+         [MBProgressHUD hideHUDForView:self.view animated:YES];
+         [self presentViewController:controller animated:YES completion:nil];
+         
+     }failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+         
+         NSString *alertMsg;
+         NSString *alertTitle;
+         UIAlertController *alertController;
+         
+         if ([operation.response statusCode] == 400) {
              
-             // TODO: save the auth token more securely in Keychain
-             NSString *authToken = responseObject[@"auth_token"];
-             NSString *userID = responseObject[@"user_id"];
-             [[NSUserDefaults standardUserDefaults] setObject:authToken forKey:@"favor8AuthToken"];
-             [[NSUserDefaults standardUserDefaults] setObject:userID forKey:@"favor8UserID"];
-             [[NSUserDefaults standardUserDefaults] synchronize];
+             alertTitle = @"User Already Exists";
+             alertMsg = @"That username is already taken. Please try something else. If you already have an account, please tap Login.";
              
-             NSString *savedVal = [[NSUserDefaults standardUserDefaults] stringForKey:@"favor8AuthToken"];
-             NSLog(@"Saved value: %@", savedVal);
+             alertController = [UIAlertController alertControllerWithTitle:alertTitle message:alertMsg preferredStyle:UIAlertControllerStyleAlert];
              
-             StackedViewController *controller = [self.storyboard instantiateViewControllerWithIdentifier:@"card"];
+             UIAlertAction *loginAction = [UIAlertAction actionWithTitle:@"Login" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                 [self performSegueWithIdentifier:@"loginFromSignupSegue" sender:self];
+             }];
              
-             [self presentViewController:controller animated:YES completion:nil];
+             UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                 [self.usernameField becomeFirstResponder];
+             }];
              
-         }failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-             NSLog(@"Error: %@", error);
+             [alertController addAction:loginAction];
+             [alertController addAction:okAction];
              
-             //dispatch_queue_t mainQueue = dispatch_get_main_queue();
-             //dispatch_async(mainQueue, ^{
-             [[[UIAlertView alloc] initWithTitle:@"Error" message:@"We're so sorry! Something went wrong in trying to sign you up :(" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil] show];
-             // });
-             [MBProgressHUD hideHUDForView:self.view animated:YES];
-         }];
-    });
+         } else {
+             
+             alertTitle = @"Oops";
+             alertMsg = @"Sorry, something went wrong and we couldn't log-in you in.";
+             
+             alertController = [UIAlertController alertControllerWithTitle:alertTitle message:alertMsg preferredStyle:UIAlertControllerStyleAlert];
+             
+             UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"Try Again" style:UIAlertActionStyleDefault handler:nil];
+             
+             [alertController addAction:okAction];
+             
+         }
+         
+         [MBProgressHUD hideHUDForView:self.view animated:YES];
+         [self presentViewController:alertController animated:YES completion:nil];
+     }];
 }
 
 # pragma mark - UITextFieldDelegate protocol methods
@@ -158,6 +181,27 @@
         return YES;
     }
 }
+
+/** Validates text fields so that action button is never
+ * enabled when text fields are empty.
+ * @return YES, if both textfields are non-empty; otherwise, NO.
+ */
+- (IBAction)validateFields:(id)sender
+{
+    BOOL valid = YES;
+    
+    NSArray *textFieldArray = @[self.usernameField, self.passwordField, self.nameField];
+    
+    // On every press we're going to run through all the fields and get their length values. If any of them equal nil we will set our bool to NO.
+    for (int i = 0; i < [textFieldArray count]; i++)
+    {
+        if (![[[textFieldArray objectAtIndex:i] text] length])
+            valid = NO;
+    }
+    
+    [self.signupButton setEnabled:valid];
+}
+
 
 /*
  #pragma mark - Navigation
